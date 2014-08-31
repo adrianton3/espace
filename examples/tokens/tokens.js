f84beb2f8659da5ad6ed445fc44b39c1b4186abc
Adrian Toncean <adrianton3@gmail.com>
2014-08-31 08:17:16 +0200

    Moved example files around

--- examples/tokens/tokens.js
+++ examples/tokens/tokens.js
@@ -9,6 +9,7 @@
 		'"\\"" "\'"'
 	].join('\n');
 
+
 	// setting up the editors
 	var inputEditor = ace.edit('input-editor');
 	inputEditor.setTheme('ace/theme/monokai');
@@ -18,29 +19,10 @@
 
 	var outputEditor = ace.edit('output-editor');
 	outputEditor.setTheme('ace/theme/monokai');
+	outputEditor.getSession().setMode('ace/mode/javascript');
 	outputEditor.getSession().setUseWrapMode(true);
 	outputEditor.setReadOnly(true);
 	outputEditor.setFontSize(18);
-
-
-	// defining how tokens should be stringified
-	var printer = {
-		'alphanum': function (token) {
-			return 'alphanum:' + token.value;
-		},
-		'number': function (token) {
-			return token.value;
-		},
-		'string': function (token) {
-			return '"' + token.value + '"';
-		},
-		'(': function (token) {
-			return '(';
-		},
-		')': function (token) {
-			return ')';
-		}
-	};
 
 
 	// getting a tokenizer
@@ -51,16 +33,14 @@
 		try {
 			var tokens = tokenizer(inputText);
 
-			var stringedTokens = tokens.map(function (token) {
-				return printer[token.type](token);
-			}).join(', ');
-
-			outputEditor.setValue(stringedTokens, 1);
+			outputEditor.setValue(JSON.stringify(tokens, undefined, 2), 1);
 		} catch (ex) {
-			outputEditor.setValue(
-				ex.message + '\nLine: ' + ex.coords.line + ' Column: ' + ex.coords.column,
-				1
-			);
+			var exceptionObject = {
+				message: ex.message,
+				coords: ex.coords
+			};
+
+			outputEditor.setValue(JSON.stringify(exceptionObject, undefined, 2), 1);
 		}
 	}
 })();
