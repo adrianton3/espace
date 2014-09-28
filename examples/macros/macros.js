8ebda1f889bfdd2b6e3754efad2ee460dfebe413
Adrian Toncean <adrianton3@gmail.com>
2014-09-28 16:04:00 +0200

    Added more code samples for the macros example

--- examples/macros/macros.js
+++ examples/macros/macros.js
@@ -1,36 +1,100 @@
 (function () {
 	'use strict';
 
-	var SAMPLE_TEXT = {
-		source: [
-			'(+ a b c)'
-		].join('\n'),
-		macros: [
-			'((+ x y z)',
-			' (+ x (+ y z)))'
-		].join('\n')
+	var SAMPLE_TEXTS = {
+		simple: {
+			source: [
+				'(+ a b c)'
+			].join('\n'),
+			macros: [
+				'(syntax-rule',
+				' (+ x y z)',
+				' (+ x (+ y z)))'
+			].join('\n')
+		},
+		nested: {
+			source: [
+				'(- (- a b))'
+			].join('\n'),
+			macros: [
+				'(syntax-rule',
+				' (- (- x y))',
+				' (- y x))'
+			].join('\n')
+		},
+		rest1: {
+			source: [
+				'(tail (list a b c d e))'
+			].join('\n'),
+			macros: [
+				'(syntax-rule',
+				' (tail (list x y...))',
+				' (list y...))'
+			].join('\n')
+		},
+		rest2: {
+			source: [
+				'(init (list a b c d e))'
+			].join('\n'),
+			macros: [
+				'(syntax-rule',
+				' (init (list x... y))',
+				' (list x...))'
+			].join('\n')
+		},
+		hygiene: {
+			source: [
+				'(swap a b)'
+			].join('\n'),
+			macros: [
+				'(syntax-rule',
+				' (swap x y)',
+				' (let (_tmp x)',
+				'  (set! x y)',
+				'  (set! y _tmp)))'
+			].join('\n')
+		}
 	};
 
+	var sourceEditor, macrosEditor, outputEditor;
 
-	// setting up the editors
-	var sourceEditor = ace.edit('source-editor');
-	sourceEditor.setTheme('ace/theme/monokai');
-	sourceEditor.setFontSize(18);
-	sourceEditor.setValue(SAMPLE_TEXT.source, 1);
-	sourceEditor.on('input', onInput);
+	function setupEditors() {
+		sourceEditor = ace.edit('source-editor');
+		sourceEditor.setTheme('ace/theme/monokai');
+		sourceEditor.setFontSize(18);
+		sourceEditor.setValue(SAMPLE_TEXTS.simple.source, 1);
+		sourceEditor.on('input', onInput);
 
-	var macrosEditor = ace.edit('macros-editor');
-	macrosEditor.setTheme('ace/theme/monokai');
-	macrosEditor.setFontSize(18);
-	macrosEditor.setValue(SAMPLE_TEXT.macros, 1);
-	macrosEditor.on('input', onInput);
+		macrosEditor = ace.edit('macros-editor');
+		macrosEditor.setTheme('ace/theme/monokai');
+		macrosEditor.setFontSize(18);
+		macrosEditor.setValue(SAMPLE_TEXTS.simple.macros, 1);
+		macrosEditor.on('input', onInput);
 
-	var outputEditor = ace.edit('output-editor');
-	outputEditor.setTheme('ace/theme/monokai');
-	outputEditor.getSession().setUseWrapMode(true);
-	outputEditor.setReadOnly(true);
-	outputEditor.setFontSize(18);
+		outputEditor = ace.edit('output-editor');
+		outputEditor.setTheme('ace/theme/monokai');
+		outputEditor.getSession().setUseWrapMode(true);
+		outputEditor.setReadOnly(true);
+		outputEditor.setFontSize(18);
+	}
 
+	function setupLinks() {
+		var selection = document.getElementsByClassName('link');
+		var elements = Array.prototype.slice.call(selection, 0);
+
+		var onClick = function (ev) {
+			var example = this.getAttribute('data-example');
+			sourceEditor.setValue(SAMPLE_TEXTS[example].source, 1);
+			macrosEditor.setValue(SAMPLE_TEXTS[example].macros, 1);
+		};
+
+		elements.forEach(function (element) {
+			element.addEventListener('click', onClick);
+		});
+	}
+
+	setupEditors();
+	setupLinks();
 
 	// getting a tokenizer
 	var tokenizer = espace.Tokenizer();
@@ -49,16 +113,18 @@
 			var sourceTree = parse(sourceText);
 
 			var macrosTree = parse(macrosText);
-			if (macrosTree.token.type !== '(' || macrosTree.children.length !== 2) {
+			if (macrosTree.token.type !== '(' ||
+				macrosTree.children.length !== 3 ||
+				macrosTree.children[0].token.value !== 'syntax-rule') {
 				throw new Error('Expected a macro definition');
 			}
 
-			espace.Expander.validatePattern(macrosTree.children[0]);
+			espace.Expander.validatePattern(macrosTree.children[1]);
 
 			espace.Expander.expand(
 				sourceTree,
-				macrosTree.children[0],
-				macrosTree.children[1]
+				macrosTree.children[1],
+				macrosTree.children[2]
 			);
 
 			outputEditor.setValue(serialize(sourceTree), 1);
